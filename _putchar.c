18a54c38e8b2cb41702181819aa6ffeec95a7175
imenmansouri93 <imennmansourii@gmail.com>
2022-07-07 16:10:26 +0100

    printf

--- _putchar.c
+++ _putchar.c
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 #include <unistd.h>
-
-=======
 #include "main.h"
->>>>>>> e4efff961ff19ece4c95838a6d2cc592c7f147a2
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
