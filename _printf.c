dde2f79086ccae42defc2e2cd5fd4646a6864280
imenmansouri93 <imennmansourii@gmail.com>
2022-07-10 13:07:06 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 	return (-1);
 	va_start(args, format);
-	while (format[i])
+	while (format[i] && format)
 	{
 		if (format[i] != '%')
 {           _putchar (*(format + i));
