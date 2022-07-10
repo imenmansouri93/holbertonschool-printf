b3b3d07e820a4e8e3b9b30973ff314a9aecbc68c
majdi aribi <majdi.developer@gmail.com>
2022-07-10 01:56:31 +0100

    fix file _putchar

--- _printf.c
+++ _printf.c
@@ -38,35 +38,3 @@ int _printf(const char *format, ...)
 	return (count);
 }
 	
-/**
- * printstring
- */
-int printstring(va_list args)
-{
-	char *str;
-	int i = 0;
-	str = va_arg(args, char *);
-	if (str == NULL)
-	{
-		str = "(null)";
-	}
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	return (1);
-}
-/**
- * printchracter
- */
-int printcharacter(va_list args)
-{    int x = 0;
-	x = va_arg(args, int);
-	_putchar(x);
-
-	return (1);
-}
-/**
- *
- */
